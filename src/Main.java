2de4ff9b1c3d3288c31efd21c1f9f8c106f5fe95
RGZ <2181855786@qq.com>
2026-09-02 17:38:42 +0800

    Day 3 Java learning

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,61 @@
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 import java.util.Scanner;
+class  Student{
+    String name;
+    int age;
+    double score;
+
+    Student(String name,int age,double score){
+        this.name=name;
+        this.age=age;
+        this.score=score;
+    }
+
+    void introduce(){
+        System.out.println("我是"+name+
+                "今年"+age+
+                "岁，成绩是"+score);
+    }
+        }
+
+    class  BankAccount{
+    String accname;
+    double balance;
+
+    BankAccount(String accname,double balance){
+        this.accname=accname;
+        this.balance=balance;
+    }
+    void deposit(double amount){
+        System.out.println("存入："+amount);
+        balance+=amount;
+        System.out.println("余额"+balance);
+    }
+    void withdraw(double amount){
+        System.out.println("取出"+amount);
+        balance-=amount;
+        if(balance>=0){
+            System.out.println("余额"+balance);
+        }else{
+            System.out.println("余额不足！");
+        }
+    }
+    void show(){
+        System.out.println("用户："+accname);
+        System.out.println("余额"+balance);
+    }
+    }
 public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int get = 0;
-        int noget = 0;
-        double average = 0;
-        double total = 0;
-        double maxx ;
-        double min;
-        System.out.println("Please enter student number:");
-        int num = scanner.nextInt();
-        double[] arr = new double[num];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextDouble();
-        }
-        maxx = arr[0];
-        min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            // 求最大值
-            if (arr[i] > maxx) {
-                maxx = arr[i];
-            }
-            // 求最小值
-            if (arr[i] < min) {
-                min = arr[i];
-            }}
-            // 累加总分
-            for (int i = 0; i < arr.length; i++) {
-                total += arr[i];
-            }
-            average = total / num;
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] > 70) {
-                    get++;
-                } else {
-                    noget++;
-                }
-            }
-            System.out.println("===== 成绩统计 ======");
-            System.out.println("最高分" + maxx);
-            System.out.println("最低分" + min);
-            System.out.println("平均分" + average);
-            System.out.println("及格人数" + get);
-            System.out.println("不及格人数" + noget);
-
+       Student student1 = new Student("景甜",38,5000.00);
+       Student student2 = new Student("张继科",37,100.01);
+       student1.introduce();;
+       student2.introduce();
+       BankAccount bankaccount1=new BankAccount("Tom",2000);
+       bankaccount1.show();
+       bankaccount1.deposit(500);
+       bankaccount1.withdraw(1000);
         }
     }
